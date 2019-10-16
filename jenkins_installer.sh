5b07b25bcb84d7734617688d40c74c06eaef7753
eyob tariku <eyobtariku@gmail.com>
2019-10-16 19:22:53 +0300

    Run docker with a `sudo` command

--- jenkins_installer.sh
+++ jenkins_installer.sh
@@ -12,10 +12,9 @@ fi
 sudo mkdir -p /var/jenkins_home
 sudo chown -R 1000:1000 /var/jenkins_home/
 
-docker run -p 8080:8080 -p 50000:50000 \
-		   -u root \
-		   -d --rm \
+sudo docker run -p 8080:8080 -p 50000:50000 \
+		   -d \
 		   -v /var/jenkins_home:/var/jenkins_home -d \
 		   -v /var/run/docker.sock:/var/run/docker.sock \
 		   --name jenkins \
-		   jenkinsci/blueocean
+		   jenkins
